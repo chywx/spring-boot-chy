9360d7d9903614c085db78a8e362e6161d020537
dahai <1559843332@qq.com>
2020-12-02 16:52:58 +0800

    kafka简单整合完毕

--- chy-kafka/src/main/java/cn/chendahai/chy/controller/KafkaProducerController.java
+++ chy-kafka/src/main/java/cn/chendahai/chy/controller/KafkaProducerController.java
@@ -6,6 +6,8 @@ package cn.chendahai.chy.controller;/**
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,37 @@ public class KafkaProducerController {
     @GetMapping("/send/simple")
     public void sendMessage1(String message) {
         kafkaTemplate.send("topic1", message);
+    }
+
+    @GetMapping("/send/callbackOne")
+    public void sendMessage2(String message) {
+        kafkaTemplate.send("topic1", message).addCallback(success -> {
+            // 消息发送到的topic
+            String topic = success.getRecordMetadata().topic();
+            // 消息发送到的分区
+            int partition = success.getRecordMetadata().partition();
+            // 消息在分区内的offset
+            long offset = success.getRecordMetadata().offset();
+            System.out.println("发送消息成功:" + topic + "-" + partition + "-" + offset);
+        }, failure -> {
+            System.out.println("发送消息失败:" + failure.getMessage());
+        });
+    }
+
+    @GetMapping("/send/callbackTwo")
+    public void sendMessage3(String message) {
+        kafkaTemplate.send("topic1", message).addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("发送消息失败：" + ex.getMessage());
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                System.out.println("发送消息成功：" + result.getRecordMetadata().topic() + "-"
+                    + result.getRecordMetadata().partition() + "-" + result.getRecordMetadata().offset());
+            }
+        });
     }
 
 }
