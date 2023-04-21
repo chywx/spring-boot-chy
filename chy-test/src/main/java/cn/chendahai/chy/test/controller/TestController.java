73efccccd172979d713c4051736b18fbfb7a2611
chenhaiyang <1559843332@qq.com>
2023-04-21 11:11:59 +0800

    springboot脚手架搭建 test

---
+++ chy-test/src/main/java/cn/chendahai/chy/test/controller/TestController.java
@@ -0,0 +1,21 @@
+package cn.chendahai.chy.test.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping("/test1")
+    public String test1(@RequestParam(defaultValue = "hello") String msg) {
+        logger.info("tset1 msg: {}", msg);
+        return "success→" + msg;
+    }
+
+}
