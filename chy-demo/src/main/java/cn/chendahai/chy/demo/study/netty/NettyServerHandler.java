941694edb429e286f783686333bdda890a03ab65
chenhaiyang <chenhaiyang@luckybus.me>
2023-11-14 15:34:03 +0800

    chy: learn netty

---
+++ chy-demo/src/main/java/cn/chendahai/chy/demo/study/netty/NettyServerHandler.java
@@ -0,0 +1,65 @@
+package cn.chendahai.chy.demo.study.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 自定义Handler需要继承netty规定好的某个HandlerAdapter(规范)
+ */
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 当客户端连接服务器完成就会触发该方法
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        System.out.println("客户端连接通道建立完成");
+    }
+
+    /**
+     * 读取客户端发送的数据
+     *
+     * @param ctx 上下文对象, 含有通道channel，管道pipeline
+     * @param msg 就是客户端发送的数据
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        //Channel channel = ctx.channel();
+        //ChannelPipeline pipeline = ctx.pipeline(); //本质是一个双向链接, 出站入站
+        //将 msg 转成一个 ByteBuf，类似NIO 的 ByteBuffer
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("收到客户端的消息:" + buf.toString(CharsetUtil.UTF_8));
+    }
+
+    /**
+     * 数据读取完毕处理方法
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ByteBuf buf = Unpooled.copiedBuffer("HelloClient".getBytes(CharsetUtil.UTF_8));
+        ctx.writeAndFlush(buf);
+    }
+
+    /**
+     * 处理异常, 一般是需要关闭通道
+     *
+     * @param ctx
+     * @param cause
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+
+}
