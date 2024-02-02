8dbc620e016bb43da2ab1feda26acffb883b6036
chenhaiyang <chenhaiyang@luckybus.me>
2024-02-02 16:41:22 +0800

    chy: 新增request模块

---
+++ chy-request/src/main/java/cn/chendahai/chy/request/SpringBootRequestApplication.java
@@ -0,0 +1,13 @@
+package cn.chendahai.chy.request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRequestApplication.class, args);
+    }
+
+}
---
+++ chy-request/src/main/java/cn/chendahai/chy/request/config/TestConfig.java
@@ -0,0 +1,9 @@
+package cn.chendahai.chy.request.config;
+
+/**
+ * @Description
+ * @Author chenhaiyang
+ * @Data 2023/4/21 11:08
+ */
+public class TestConfig {
+}
