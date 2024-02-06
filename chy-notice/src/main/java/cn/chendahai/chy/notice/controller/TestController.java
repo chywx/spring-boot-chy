52292d927e595a34a1a722e099077ba5241a529f
chenhaiyang <chenhaiyang@luckybus.me>
2024-02-06 13:41:36 +0800

    chy: 新增notice模块

---
+++ chy-notice/src/main/java/cn/chendahai/chy/notice/controller/TestController.java
@@ -0,0 +1,24 @@
+package cn.chendahai.chy.notice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    private Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping("/getApplicationName")
+    public String getApplicationName() {
+        logger.info("application name: {}", applicationName);
+        return "success→" + applicationName;
+    }
+
+}
