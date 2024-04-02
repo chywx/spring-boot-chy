c50dedd1faf7d54632db666e00447dcb2e3ce314
chenhaiyang <chenhaiyang@luckybus.me>
2024-04-02 15:08:02 +0800

    chy: 修改smpp模块

--- chy-smpp/src/main/java/cn/chendahai/chy/pp/config/client/SmppClient.java
+++ chy-smpp/src/main/java/cn/chendahai/chy/pp/config/client/SmppClient.java
@@ -3,9 +3,15 @@ package cn.chendahai.chy.pp.config.client;
 import com.cloudhopper.smpp.SmppSession;
 import com.cloudhopper.smpp.SmppSessionConfiguration;
 import com.cloudhopper.smpp.impl.DefaultSmppClient;
+import com.cloudhopper.smpp.impl.DefaultSmppSessionHandler;
+import com.cloudhopper.smpp.pdu.PduRequest;
+import com.cloudhopper.smpp.pdu.PduResponse;
 import com.cloudhopper.smpp.pdu.SubmitSm;
+import com.cloudhopper.smpp.pdu.SubmitSmResp;
 import com.cloudhopper.smpp.type.Address;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class SmppClient {
     public static void main(String[] args) throws Exception {
         SmppSessionConfiguration config = new SmppSessionConfiguration();
@@ -15,22 +21,45 @@ public class SmppClient {
         config.setPassword("1qaz2wsx");
 
         DefaultSmppClient client = new DefaultSmppClient();
-        SmppSession session = client.bind(config, new MySmppSessionHandler());
 
-        session = client.bind(config, new MySmppSessionHandler());
+        SmppSession session = client.bind(config, new ClientSmppSessionHandler());
 
         // Prepare and send the "Hello, World!" message
-        for (int i = 0; i < 120; i++) {
+        for (int i = 0; i < 2; i++) {
             SubmitSm sm = new SubmitSm();
             sm.setSourceAddress(new Address((byte) 0x03, (byte) 0x00, "SENDER"));
             sm.setDestAddress(new Address((byte) 0x01, (byte) 0x01, "RECIPIENT"));
-            sm.setShortMessage("Hello, World!".getBytes());
-            session.submit(sm, 10000);
+            sm.setShortMessage(new String("Hello, World!" + i).getBytes());
+            SubmitSmResp submitSmResp = session.submit(sm, 10000);
+            log.info("result {} {}", i, submitSmResp);
         }
 
         // 关闭会话
-        if (session != null && session.isBound()) {
+        if (session.isBound()) {
             session.unbind(5000);
         }
     }
+
+    public static class ClientSmppSessionHandler extends DefaultSmppSessionHandler {
+
+        public ClientSmppSessionHandler() {
+            super(log);
+        }
+
+        @Override
+        public void firePduRequestExpired(PduRequest pduRequest) {
+            log.warn("PDU request expired: {}", pduRequest);
+        }
+
+        @Override
+        public PduResponse firePduRequestReceived(PduRequest pduRequest) {
+            PduResponse response = pduRequest.createResponse();
+
+            // do any logic here
+
+            return response;
+        }
+
+    }
+
 }
--- chy-smpp/src/main/java/cn/chendahai/chy/pp/config/server/MySmppServerSessionHandler.java
+++ chy-smpp/src/main/java/cn/chendahai/chy/pp/config/server/MySmppServerSessionHandler.java
@@ -2,29 +2,64 @@ package cn.chendahai.chy.pp.config.server;
 
 import com.cloudhopper.smpp.SmppServerHandler;
 import com.cloudhopper.smpp.SmppServerSession;
+import com.cloudhopper.smpp.SmppSession;
 import com.cloudhopper.smpp.SmppSessionConfiguration;
-import com.cloudhopper.smpp.impl.DefaultSmppSession;
+import com.cloudhopper.smpp.impl.DefaultSmppSessionHandler;
 import com.cloudhopper.smpp.pdu.BaseBind;
 import com.cloudhopper.smpp.pdu.BaseBindResp;
+import com.cloudhopper.smpp.pdu.PduRequest;
+import com.cloudhopper.smpp.pdu.PduResponse;
 import com.cloudhopper.smpp.type.SmppProcessingException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MySmppServerSessionHandler implements SmppServerHandler {
     @Override
-    public void sessionBindRequested(Long aLong, SmppSessionConfiguration smppSessionConfiguration, BaseBind baseBind) throws SmppProcessingException {
-        log.info("sessionBindRequested");
+    public void sessionBindRequested(Long sessionId, SmppSessionConfiguration smppSessionConfiguration, BaseBind baseBind) throws SmppProcessingException {
+
+        log.info("sessionBindRequested reqId:{}", sessionId);
+        log.info("baseBind {}", baseBind);
+        smppSessionConfiguration.setName("chy.smpp." + smppSessionConfiguration.getSystemId());
     }
 
     @Override
     public void sessionCreated(Long aLong, SmppServerSession smppServerSession, BaseBindResp baseBindResp) throws SmppProcessingException {
-        DefaultSmppSession defaultSmppSession = (DefaultSmppSession) smppServerSession;
-//        defaultSmppSession.serverReady(defaultSmppSession);
-        log.info("sessionCreated");
+        log.info("sessionCreated reqId:{}", aLong);
+        log.info("baseBindResp {}", baseBindResp);
+        smppServerSession.serverReady(new TestSmppSessionHandler(smppServerSession));
     }
 
     @Override
     public void sessionDestroyed(Long aLong, SmppServerSession smppServerSession) {
-        log.info("sessionDestroyed");
+        log.info("Session destroyed: {}", smppServerSession);
+        if (smppServerSession.hasCounters()) {
+            log.info("final session rx-submitSM: {}", smppServerSession.getCounters().getRxSubmitSM());
+        }
+        smppServerSession.destroy();
     }
+
+    public static class TestSmppSessionHandler extends DefaultSmppSessionHandler {
+
+        private SmppSession smppSession;
+
+        public TestSmppSessionHandler(SmppSession session) {
+            this.smppSession = session;
+        }
+
+        @Override
+        public PduResponse firePduRequestReceived(PduRequest pduRequest) {
+            System.out.println("firePduRequestReceived:" + smppSession);
+
+            // mimic how long processing could take on a slower smsc
+            try {
+                //Thread.sleep(50);
+            } catch (Exception e) {
+            }
+
+            return pduRequest.createResponse();
+        }
+    }
+
 }
+
+
